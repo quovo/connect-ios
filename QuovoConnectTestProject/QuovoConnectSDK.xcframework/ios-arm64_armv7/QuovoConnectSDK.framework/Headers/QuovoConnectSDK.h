482d135b485253fd620c85e491ace11f4fed8efc
Alan Carino <alan.carino@quovo.com>
2020-11-24 21:51:07 -0500

    updated to support xcode 12, swift 5.3, distributed builds using xcframework (#34)

---
+++ QuovoConnectTestProject/QuovoConnectSDK.xcframework/ios-arm64_armv7/QuovoConnectSDK.framework/Headers/QuovoConnectSDK.h
@@ -0,0 +1,18 @@
+//
+//  QuovoConnectSDK.h
+//  QuovoConnectSDK
+//
+//  Copyright © 2017 Quovo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuovoConnectSDK.
+FOUNDATION_EXPORT double QuovoConnectSDKVersionNumber;
+
+//! Project version string for QuovoConnectSDK.
+FOUNDATION_EXPORT const unsigned char QuovoConnectSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuovoConnectSDK/PublicHeader.h>
+
+
